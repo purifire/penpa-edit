c169309369ae4f5561667ac44515b31bc6d8bbf6
purifire <purifire15@gmail.com>
2020-06-24 00:40:30 +0530

    Update general.js

--- docs/js/general.js
+++ docs/js/general.js
@@ -15,8 +15,8 @@ function boot(){
 
 function boot_parameters(){
   document.getElementById("gridtype").value = "square";
-  document.getElementById("nb_size1").value = 10;
-  document.getElementById("nb_size2").value = 10;
+  document.getElementById("nb_size1").value = 9;
+  document.getElementById("nb_size2").value = 9;
   document.getElementById("nb_size3").value = 38;
   document.getElementById("nb_space1").value = 0;
   document.getElementById("nb_space2").value = 0;
