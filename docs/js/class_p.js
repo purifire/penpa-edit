e72a170296b0b0a9101dbb7b69c4681d351779f8
purifire <purifire15@gmail.com>
2020-06-24 16:50:49 +0530

    Update class_p.js

--- docs/js/class_p.js
+++ docs/js/class_p.js
@@ -3334,7 +3334,7 @@ class Puzzle {
             var text = JSON.stringify(this.make_solution());
             if (text === this.solution && this.sol_flag === 0) {
                 setTimeout(() => {
-                    alert("Correct Answer")
+                    alert("You did it!")
                 }, 10)
                 this.sol_flag = 1;
             } else if (text != this.solution && this.sol_flag === 1) { //答えが変わったら改めて判定
